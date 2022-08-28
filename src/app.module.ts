39326fb3b01f33d097d7873883a5bb13dc93e393
Mohamad Kamar <mohamad.kamar@postlight.com>
2022-08-28 19:44:00 +0300

    feat: add database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConsumerModule } from './consumer/consumer.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ConsumerModule],
+  imports: [ConsumerModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
