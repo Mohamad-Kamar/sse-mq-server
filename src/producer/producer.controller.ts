64bfa830e0f1251bc57d5efbd7452b0bcd10d36a
Mohamad Kamar <mohamad.kamar@postlight.com>
2022-10-15 17:37:10 +0300

    refactor: remove unneeded producer endpoints

--- src/producer/producer.controller.ts
+++ src/producer/producer.controller.ts
@@ -1,15 +1,5 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { ProducerService } from './producer.service';
-import { CreateProducerDto } from './dto/create-producer.dto';
-import { UpdateProducerDto } from './dto/update-producer.dto';
 
 @Controller('producer')
 export class ProducerController {
@@ -18,28 +8,5 @@ export class ProducerController {
   @Post()
   publish(@Body() messageBody: any) {
     return this.producerService.publish(messageBody);
-  }
-
-  @Get()
-  findAll() {
-    return this.producerService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.producerService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateProducerDto: UpdateProducerDto,
-  ) {
-    return this.producerService.update(+id, updateProducerDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.producerService.remove(+id);
   }
 }
--- src/producer/producer.service.ts
+++ src/producer/producer.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PublisherMessage } from 'src/globalTypes/publisherMessage';
 import { QueueService } from '../queue/queue.service';
-import { CreateProducerDto } from './dto/create-producer.dto';
-import { UpdateProducerDto } from './dto/update-producer.dto';
 
 @Injectable()
 export class ProducerService {
@@ -16,25 +14,5 @@ export class ProducerService {
     }
     const dataTo = new MessageEvent('message', { data: message });
     assocQueue.assocObs.next(dataTo);
-  }
-
-  create(createProducerDto: CreateProducerDto) {
-    return 'This action adds a new producer';
-  }
-
-  findAll() {
-    return `This action returns all producer`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} producer`;
-  }
-
-  update(id: number, updateProducerDto: UpdateProducerDto) {
-    return `This action updates a #${id} producer`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} producer`;
   }
 }
