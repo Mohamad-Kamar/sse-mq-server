e25f8836abee5cb115caac865662d431766d436f
Mohamad Kamar <mohamad.kamar@postlight.com>
2022-10-16 16:14:18 +0300

    feat: improve fanout queue implementation

--- src/structures/Queues/FanoutQueue.ts
+++ src/structures/Queues/FanoutQueue.ts
@@ -1,24 +1,32 @@
 import { BehaviorSubject } from 'rxjs';
 import { CreateQueueDto } from 'src/queue/dto/create-queue.dto';
 import { IQueue } from './IQueue';
+import { Consumer, Consumers } from 'src/Types';
+import { v4 as uuidv4 } from 'uuid';
 
 export class FanoutQueue implements IQueue {
-  consumers: Array<BehaviorSubject<MessageEvent>>;
+  consumers: Consumers;
   queueDetails: CreateQueueDto;
   constructor(queueDetails: CreateQueueDto) {
     this.queueDetails = queueDetails;
-    this.consumers = [];
+    this.consumers = {};
   }
 
-  addConsumer() {
+  addConsumer(): Consumer {
     const messageToSend = new MessageEvent('message');
-    const addedQueue = new BehaviorSubject(messageToSend);
-    this.consumers.push(addedQueue);
-    return addedQueue;
+    const addedSubject = new BehaviorSubject(messageToSend);
+    const addedID = uuidv4();
+    const addedConsumer: Consumer = {
+      subject: addedSubject,
+      id: addedID,
+    };
+    this.consumers[addedID] = addedConsumer;
+    return addedConsumer;
   }
   publish(message: string) {
-    this.consumers.forEach((consumer) => {
-      consumer.next(new MessageEvent('message', { data: message }));
+    Object.keys(this.consumers).forEach((consumerID) => {
+      const consumerSubject = this.consumers[consumerID].subject;
+      consumerSubject.next(new MessageEvent('message', { data: message }));
     });
   }
 }
