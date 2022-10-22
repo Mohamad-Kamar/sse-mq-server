d31e409d279afc5d2b1b11e723c02196ea6ef66c
Mohamad Kamar <mohamad.kamar@postlight.com>
2022-10-23 01:01:15 +0300

    feat: add config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConsumerModule } from './consumer/consumer.module';
+import { ConfigModule } from '@nestjs/config';
 import { ProducerModule } from './producer/producer.module';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [ConsumerModule, ProducerModule],
+  imports: [
+    ConsumerModule,
+    ProducerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      cache: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
