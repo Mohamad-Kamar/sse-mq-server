30e49473abfd88ca776e959a22047faac2ee50b5
Mohamad-Kamar <mohamad.kamar.dev@gmail.com>
2023-01-17 00:52:46 +0200

    feat: cleanup database service

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CreateDatabaseDto } from './dto/create-database.dto';
-import { UpdateDatabaseDto } from './dto/update-database.dto';
 import { IQueue } from '../queue/entities/IQueue';
 import { DBQueues, DBQueue } from '../Types';
 
@@ -24,24 +22,5 @@ export class DatabaseService {
     this.queues[queueKey] = newQueue;
     return newQueue;
   }
-
-  create(createDatabaseDto: CreateDatabaseDto) {
-    return 'This action adds a new database';
-  }
-
-  findAll() {
-    return `This action returns all database`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} database`;
-  }
-
-  update(id: number, updateDatabaseDto: UpdateDatabaseDto) {
-    return `This action updates a #${id} database`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} database`;
-  }
+  reset() {}
 }
