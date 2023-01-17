cb15ce21da49d048203f82be1b6f58327aa14756
Mohamad-Kamar <mohamad.kamar.dev@gmail.com>
2023-01-17 18:57:19 +0200

    feat: improve consumer functionality

--- src/consumer/consumer.service.ts
+++ src/consumer/consumer.service.ts
@@ -1,49 +1,71 @@
 /* eslint-disable prefer-const */
 import { Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
-import { QueueService } from '../queue/queue.service';
 import { CreateConsumerDto } from './dto/create-consumer.dto';
 import { UpdateConsumerDto } from './dto/update-consumer.dto';
 import { InvalidQueueError } from '../structures/Errors/InvalidQueueError';
 import { QueueNotFoundError } from '../structures/Errors/QueueNotFoundError';
 import { ConnectToQueueDto } from '../queue/dto/connect-to-queue';
-import { InvalidConsumerError } from '../structures/Errors/InvalidConsumerError';
+import { DatabaseService } from 'src/database/database.service';
+import { InstanceConsumerCollection } from 'src/Types';
+import { InstaceConsumer } from './dto/instance-consumer.dto';
+import { v4 as uuidv4 } from 'uuid';
+import { AlreadyExistsError } from 'src/structures/Errors/AlreadyExistsError';
 
 @Injectable()
 export class ConsumerService {
-  constructor(private readonly queueService: QueueService) {}
+  consumers: InstanceConsumerCollection;
+  constructor(private readonly databaseService: DatabaseService) {
+    this.consumers = this.databaseService.getConsumers();
+  }
 
   connect(connectToQueueDto: ConnectToQueueDto): Observable<MessageEvent> {
     let { queueKey, consumerID } = connectToQueueDto;
     if (!queueKey) throw new InvalidQueueError('Queue Key Missing');
 
-    const targetQueue = this.queueService.getQueue(queueKey);
-    let targetConsumer = targetQueue.queue.getConsumer(consumerID);
+    let targetConsumer = this.findOne(consumerID);
     if (!targetConsumer) {
-      targetQueue.queue.addConsumer(connectToQueueDto);
-      targetConsumer = targetQueue.queue.getConsumer(consumerID);
+      this.create(connectToQueueDto);
+      targetConsumer = this.findOne(consumerID);
     }
     return targetConsumer.consumerSubject;
   }
 
-  create(createConsumerDto: CreateConsumerDto): string {
+  create(createConsumerDto: CreateConsumerDto): CreateConsumerDto {
     const { queueKey }: CreateConsumerDto = createConsumerDto;
     if (!queueKey) throw new InvalidQueueError('Queue Key Missing');
 
-    const assocQueue = this.queueService.getQueue(queueKey);
+    const assocQueue = this.databaseService.getQueue(queueKey);
     if (!assocQueue) {
       throw new QueueNotFoundError();
     }
-    const targetConsumerID = assocQueue.queue.addConsumer(createConsumerDto);
-    return targetConsumerID;
+
+    if (!createConsumerDto.consumerID) createConsumerDto.consumerID = uuidv4();
+    if (this.findOne(createConsumerDto.consumerID))
+      throw new AlreadyExistsError();
+
+    let consumerInstace = new InstaceConsumer(
+      createConsumerDto.queueKey,
+      createConsumerDto.consumerID,
+    );
+
+    this.databaseService.saveConsumer(createConsumerDto);
+    assocQueue.addConsumer(consumerInstace);
+    this.addConsumer(consumerInstace);
+
+    return createConsumerDto;
+  }
+
+  addConsumer(consumerInstace: InstaceConsumer) {
+    this.consumers[consumerInstace.consumerID] = consumerInstace;
+  }
+
+  findOne(consumerID: string): InstaceConsumer {
+    return this.consumers[consumerID];
   }
 
   findAll() {
     return `This action returns all consumer`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} consumer`;
   }
 
   update(id: number, updateConsumerDto: UpdateConsumerDto) {
--- src/consumer/dto/instance-consumer.dto.ts
+++ src/consumer/dto/instance-consumer.dto.ts
@@ -1,7 +1,34 @@
 import { ReplaySubject } from 'rxjs';
+import {
+  Message,
+  MessageCollection,
+} from 'src/Storage/IStorage/IStorage_Types';
 
-export class InstaceConsumerDto {
+export class InstaceConsumer {
   queueKey: string;
   consumerID: string;
-  consumerSubject?: ReplaySubject<MessageEvent>;
+  messages: MessageCollection;
+  consumerSubject: ReplaySubject<MessageEvent>;
+  constructor(
+    queueKey: string,
+    consumerID: string,
+    messages: MessageCollection = {},
+  ) {
+    this.queueKey = queueKey;
+    this.consumerID = consumerID;
+    this.messages = messages;
+    this.consumerSubject = new ReplaySubject();
+  }
+
+  getMessages() {
+    return this.messages;
+  }
+
+  setMessages(messages: MessageCollection) {
+    this.messages = messages;
+  }
+
+  addMessage(message: Message) {
+    this.messages[message.messageID] = message;
+  }
 }
