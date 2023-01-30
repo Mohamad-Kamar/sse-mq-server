95a892beaf51cf0f1e49bb75723aaa3d995da39d
Mohamad-Kamar <mohamad.kamar.dev@gmail.com>
2023-01-30 20:06:19 +0200

    docs: add comment for using config sesrvice

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  // Use Port from environment variables
   const configService = app.get(ConfigService);
   const port = configService.get('port');
 
